b99bbeeed7d1caf9c859469e98bd2cfd1f258162
yliu996 <yliu996@github.gatech.edu>
2020-02-15 00:17:45 -0500

    update

--- oauth-authorization-server/src/main/java/com/baeldung/config/AuthorizationServerApplication.java
+++ oauth-authorization-server/src/main/java/com/baeldung/config/AuthorizationServerApplication.java
@@ -3,7 +3,9 @@ package com.baeldung.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication(scanBasePackages = "com.baeldung")
 public class AuthorizationServerApplication extends SpringBootServletInitializer {
 
--- oauth-zuul-gateway/src/main/java/org/baeldung/GatewayApplication.java
+++ oauth-zuul-gateway/src/main/java/org/baeldung/GatewayApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableResourceServer
 @EnableZuulProxy
 @SpringBootApplication
+@EnableEurekaClient
 public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
